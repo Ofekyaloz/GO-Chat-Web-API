31c8da734fe34d85dae8414514c281372be12a9a
gilig <giligutfeld@gmail.com>
2022-03-30 18:47:34 +0300

    Added little css

--- src/SignIn.js
+++ src/SignIn.js
@@ -2,10 +2,10 @@ function SignIn() {
     return (
         <div className="App">
             <header className="App-header">
-                <div>
+                <div className={"col-3"}>
                     <h1>Log Into GoChat</h1>
                     <form>
-                        <div className="mb-3">
+                        <div className="mb-3 paddingUp">
                             <input type="email" className="form-control" id="exampleInputEmail1"
                                    aria-describedby="emailHelp" placeholder={"Email address"}></input>
                         </div>
@@ -13,21 +13,21 @@ function SignIn() {
                             <input type="password" className="form-control" id="exampleInputPassword1"
                                    placeholder={"Password"}></input>
                         </div>
-                        <div className="d-grid gap-2 col-12 mx-auto">
-                            <button type="submit" className="btn btn-primary">Log In</button>
-                        </div>
                         <div className="checkbox mb-3">
                             <label>
                                 <input type={"checkbox"} value={"remember-me"}></input> Remember me
                             </label>
                         </div>
+                        <div className="d-grid gap-2 col-12 mx-auto">
+                            <button type="submit" className="btn btn-primary">Log In</button>
+                        </div>
                     </form>
-                    <div>
-                        <a href="#" className="link-primary">
-                            <label>Forgot account?</label>
+                    <div className={"paddingUp col-12"}>
+                        <a href="#" className="link-primary col-6 leftSide">
+                            <label className={"fontSize"}>Forgot account?</label>
                         </a>
-                        <a href="SignUp" className="link-primary">
-                            <label>Sign up for GoChat</label>
+                        <a href="SignUp " className="link-primary col-6 rightSide">
+                            <label className={"fontSize"}>Sign up for GoChat</label>
                         </a>
                     </div>
                 </div>
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,93 +1,45 @@
-function SignIn() {
+import './SignUp.css';
+
+function SignUp() {
     return (
         <div className="App">
             <header className="App-header">
-                <div>
-                    <h1>Log Into GoChat</h1>
-                    <form className="row g-3">
-                        <div className="col-md-4">
-                            <label htmlFor="validationServer01" className="form-label">First name</label>
-                            <input type="text" className="form-control is-valid" id="validationServer01" placeholder="First name"
-                                   required></input>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <label htmlFor="validationServer02" className="form-label">Last name</label>
-                            <input type="text" className="form-control is-valid" id="validationServer02" value="Otto"
-                                   required></input>
-                            <div className="valid-feedback">
-                                Looks good!
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <label htmlFor="validationServerUsername" className="form-label">Username</label>
-                            <div className="input-group has-validation">
-                                <span className="input-group-text" id="inputGroupPrepend3">@</span>
-                                <input type="text" className="form-control is-invalid" id="validationServerUsername"
-                                       aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback"
-                                       required></input>
-                                <div id="validationServerUsernameFeedback" className="invalid-feedback">
-                                    Please choose a username.
-                                </div>
-                            </div>
+                <div className={"col-3"}>
+                    <label>Sign up to GoChat</label>
+                    <form className="row g-3 paddingUp">
+                        <div className="col-md-6">
+                            <input type="text" className="form-control" id="inputUsername4"
+                                   placeholder={"Username"} required></input>
                         </div>
                         <div className="col-md-6">
-                            <label htmlFor="validationServer03" className="form-label">City</label>
-                            <input type="text" className="form-control is-invalid" id="validationServer03"
-                                   aria-describedby="validationServer03Feedback" required></input>
-                            <div id="validationServer03Feedback" className="invalid-feedback">
-                                Please provide a valid city.
-                            </div>
+                            <input type="text" className="form-control" id="inputNickname4"
+                                   placeholder={"Nickname"} required></input>
                         </div>
-                        <div className="col-md-3">
-                            <label htmlFor="validationServer04" className="form-label">State</label>
-                            <select className="form-select is-invalid" id="validationServer04"
-                                    aria-describedby="validationServer04Feedback" required>
-                                <option selected disabled value="">Choose...</option>
-                                <option>...</option>
-                            </select>
-                            <div id="validationServer04Feedback" className="invalid-feedback">
-                                Please select a valid state.
-                            </div>
+                        <div className="col-md-12">
+                            <input type="email" className="form-control" id="inputEmail4" placeholder={"Email"}
+                                   required></input>
                         </div>
-                        <div className="col-md-3">
-                            <label htmlFor="validationServer05" className="form-label">Zip</label>
-                            <input type="text" className="form-control is-invalid" id="validationServer05"
-                                   aria-describedby="validationServer05Feedback" required></input>
-                            <div id="validationServer05Feedback" className="invalid-feedback">
-                                Please provide a valid zip.
-                            </div>
+                        <div className="col-md-6">
+                            <input type="password" className="form-control" id="inputPassword4"
+                                   placeholder={"Password"} required></input>
                         </div>
-                        <div className="col-12">
-                            <div className="form-check">
-                                <input className="form-check-input is-invalid" type="checkbox" value=""
-                                       id="invalidCheck3" aria-describedby="invalidCheck3Feedback" required></input>
-                                <label className="form-check-label" htmlFor="invalidCheck3">
-                                    Agree to terms and conditions
-                                </label>
-                                <div id="invalidCheck3Feedback" className="invalid-feedback">
-                                    You must agree before submitting.
-                                </div>
-                            </div>
+                        <div className="col-md-6">
+                            <input type="password" className="form-control" id="inputPasswordAgain"
+                                   placeholder={"Password again"} required></input>
                         </div>
-                        <div className="col-12">
-                            <button className="btn btn-primary" type="submit">Submit form</button>
+                        <div className="col-md-12 paddingUp">
+                            <div className={"col-6"}>
+                                <a href={"/"} className="link-primary col-6 leftSide">Sign in</a>
+                            </div>
+                            <div className={"col-6"}>
+                                <button type="submit" className="btn btn-primary rightSide">Submit</button>
+                            </div>
                         </div>
                     </form>
-                    <div>
-                        <a href="#" className="link-primary">
-                            <label>Fot account?</label>
-                        </a>
-                        <a href="SignUp.js" className="link-primary">
-                            <label>Sign up for GoChat</label>
-                        </a>
-                    </div>
                 </div>
             </header>
         </div>
     );
 }
 
-export default SignIn;
+export default SignUp;
