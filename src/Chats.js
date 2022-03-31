3c16ca0b362eb6592ff48022918c41331afcf1b1
ofek1 <ofekyaloz16@gmail.com>
2022-03-31 18:34:26 +0300

    search button

--- src/Chats.js
+++ src/Chats.js
@@ -6,18 +6,17 @@ function Chats() {
             <div className={"row"}>
                 <div className={"col-4"}>
                     <div id={"SearchUser"} className={"input-group flex-nowrap"}>
-                        {/*search icon*/}
-                        <span className={"input-group-text"} id={"addon-wrapping"}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             className="bi bi-search" viewBox="0 0 16 16">
-                        <path
-                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                        </svg>
-                    </span>
+                        <button className="btn btn-outline-secondary" type="button" id="searchbutton">
+                            {/*search icon*/}
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-search" viewBox="0 0 16 16">
+                                <path
+                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </button>
                         <input type="text" className={"form-control"} placeholder="Username" aria-label="Username"
                                aria-describedby="addon-wrapping" maxLength={30}></input>
                     </div>
-
 
                     <div className="list-group ChatHistory align-items-center" id="list-tab" role="tablist">
                         <a className="list-group-item list-group-item-action m-1" id="list-home-list"
@@ -51,23 +50,39 @@ function Chats() {
                                 </svg>
                             </button>
                             <ul className="dropdown-menu">
-                                {/*image icon*/}
-                                <li><a className={"dropdown-menu"}></a>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                         className="bi bi-image" viewBox="0 0 16 16">
-                                        <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                                        <path
-                                            d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
-                                    </svg>
+                                <li>
+                                    <button type="button" className="dropdown-item" href="#" id="imagebutton">
+                                        {/*image icon*/}
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                             fill="currentColor"
+                                             className="bi bi-image" viewBox="0 0 16 16">
+                                            <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+                                            <path
+                                                d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+                                        </svg>
+                                        photo
+                                    </button>
+                                </li>
+                                <li>
+                                    <button type="button" className="dropdown-item" href="#" id="videobutton">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                             fill="currentColor"
+                                             className="bi bi-camera-video" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                  d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z"/>
+                                        </svg>
+                                        video
+                                    </button>
                                 </li>
                             </ul>
                         </div>
 
                         <input type="text" className="form-control" placeholder="Type a message..."
                                aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+                        <button className="btn btn-outline-secondary" type="button" id="imagebutton">
                             {/*mic icon*/}
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-mic-fill"
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                 className="bi bi-mic-fill"
                                  viewBox="0 0 16 16">
                                 <path d="M5 3a3 3 0 0 1 6 0v5a3 3 0 0 1-6 0V3z"/>
                                 <path
@@ -75,7 +90,7 @@ function Chats() {
                             </svg>
                         </button>
 
-                        <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+                        <button className="btn btn-outline-secondary" type="button" id="sendbutton">
                             {/*message icon*/}
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  className="bi bi-chat" viewBox="0 0 16 16">
