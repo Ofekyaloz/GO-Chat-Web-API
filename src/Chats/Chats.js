57b2e2c1b3ed4fb7cc229d97e041f54f6e012d61
ofek1 <ofekyaloz16@gmail.com>
2022-04-03 12:53:51 +0300

    fix usersearch added contacts history

--- src/Chats/Chats.js
+++ src/Chats/Chats.js
@@ -9,28 +9,189 @@ function Chats({username}) {
             <div className={"row"}>
                 <div className={"col-4 vh-100 d-flex"}>
 
-                    <div id={"UserInfo"} className={"d-flex"}>
-                        <img className="UserImage" src={img1} alt=""/>
-                        <span className="ContactName m-3"> Ofek </span>
-
-                        {/*<button className="btn btn-outline-secondary" type="button" id="SearchButton">*/}
-                        {/*    /!*search icon*!/*/}
-                        {/*    <i className="bi bi-search"></i>*/}
-                        {/*</button>*/}
-
-                        {/*<input id="SearchUser" type="text" className={"form-control"} placeholder="Username"*/}
-                        {/*       aria-label="Username"*/}
-                        {/*       aria-describedby="addon-wrapping" maxLength={35}>*/}
-                        {/*</input>*/}
-
-                        <button type="button" className="btn btn-secondary" aria-expanded="false" id="NewContact">
-                            {/*contact icon*/}
-                            <i className="bi bi-person-plus-fill"/>
-                        </button>
-                    </div>
-
-
                     <div className="list-group d-flex ChatHistory" id="list-tab" role="tablist">
+
+                        <div id={"UserInfo"} className={"d-flex"}>
+                            <img className="UserImage" src={img1} alt=""/>
+                            <span className="ContactName" id="UserName"> Ofek Yaloz</span>
+
+                            <button className="btn btn-outline" type="button" id="SearchButton">
+                                {/*search icon*/}
+                                <i className="bi bi-search"></i>
+                            </button>
+
+                            <input id="SearchUser" type="text" className={"form-control"} placeholder="@Username"
+                                   aria-label="Username"
+                                   aria-describedby="addon-wrapping" maxLength={35}>
+                            </input>
+
+                            <button type="button" className="btn btn-outline" id="NewContact">
+                                {/*contact icon*/}
+                                <i className="bi bi-person-plus-fill"/>
+                            </button>
+
+                        </div>
+
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Ofek</div>
+                                            <div className="col-4"> Yesterday</div>
+                                        </div>
+                                        <div> something</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Gili</div>
+                                            <div className="col-4"> 2 days ago</div>
+                                        </div>
+                                        <div> something</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Mom</div>
+                                            <div className="col-4"> 3 days ago</div>
+                                        </div>
+                                        <div> Goodbye.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Ofek</div>
+                                            <div className="col-4"> Yesterday</div>
+                                        </div>
+                                        <div> something</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Gili</div>
+                                            <div className="col-4"> 2 days ago</div>
+                                        </div>
+                                        <div> something</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Mom</div>
+                                            <div className="col-4"> 3 days ago</div>
+                                        </div>
+                                        <div> Goodbye.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Ofek</div>
+                                            <div className="col-4"> Yesterday</div>
+                                        </div>
+                                        <div> something</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Gili</div>
+                                            <div className="col-4"> 2 days ago</div>
+                                        </div>
+                                        <div> something</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+
+                        <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
+                           role="tab" aria-controls="list-home">
+                            <div>
+                                <div className="row">
+                                    <div className="col-4">
+                                        <img className="UserImage" src={img1}/>
+                                    </div>
+                                    <div className="col-8">
+                                        <div className="row">
+                                            <div className="col-8 d-flex ContactName"> Mom</div>
+                                            <div className="col-4"> 3 days ago</div>
+                                        </div>
+                                        <div> Goodbye.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
                         <a className="list-group-item list-group-item-action" data-bs-toggle="list" href=""
                            role="tab" aria-controls="list-home">
                             <div>
