e6cb77fc8ff45813d344e4cec46f2f7f63cf0f61
ofek1 <ofekyaloz16@gmail.com>
2022-04-22 17:50:08 +0300

    chat list

--- src/Chats/ChatMsgs.js
+++ src/Chats/ChatMsgs.js
@@ -1,28 +1,18 @@
 function ChatMsgs({MessageList}) {
 
-    function adding(msg) {
-        if (msg.message.type === "photo" || msg.message.type === "video") {
-            return (
-                <>
-                    <li className={"space"}/>
-                </>
-            );
-        }
-    }
-
     return (
         <ul id="chat-msgs" className="Chat">
             {MessageList.map((msg, key) => (
-                <>
-                    <li className={msg.message.myMsg ? "right-msg" : "left-msg"} key={key}>
-                        <div>{msg.message.content} </div>
+                <li className={msg.message.myMsg ? "right-msg" : "left-msg"} key={key}>
+                    <div className={msg.message.myMsg ? "right-content" : "left-content"} key={key}>
+                        <div>
+                            {msg.message.content}
+                        </div>
                         <div className="msg-date">
                             {msg.message.date.getHours()}:{msg.message.date.getMinutes() < 10 ? '0' : ''}{msg.message.date.getMinutes()}
                         </div>
-                    </li>
-                    <br/><br/><br/>
-                    {/*{adding(msg)}*/}
-                </>
+                    </div>
+                </li>
             ))}
         </ul>
     )
