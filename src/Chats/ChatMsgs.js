64af6165aaba82b027eb28fd4eaf5fe5b975a8be
ofek1 <ofekyaloz16@gmail.com>
2022-04-22 18:45:21 +0300

    chat list

--- src/Chats/ChatMsgs.js
+++ src/Chats/ChatMsgs.js
@@ -3,7 +3,8 @@ function ChatMsgs({MessageList}) {
     return (
         <ul id="chat-msgs" className="Chat">
             {MessageList.map((msg, key) => (
-                <li className={msg.message.myMsg ? "right-msg" : "left-msg"} key={key}>
+                <li className={msg.message.myMsg ? "right-msg" : "left-msg"} key={key}
+                    style={msg.message.type === "photo" || msg.message.type === "video" ? {height: "270px"} : {height: "auto"}}>
                     <div className={msg.message.myMsg ? "right-content" : "left-content"} key={key}>
                         <div>
                             {msg.message.content}
