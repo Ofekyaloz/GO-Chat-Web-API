d5d0803910d362055847460bc92b1314cee549e9
gilig <giligutfeld@gmail.com>
2022-04-24 18:10:12 +0300

    Added chat with Omer Adam

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Message from "./Chats/Message";
 import bibi from "./Pictures/Benjamin Netanyahu.jpg";
 import eden from "./Pictures/Eden Hason.jpg";
 import omer from "./Pictures/Omer Adam.webp";
+import song from "./Pictures/bb.mp3"
 
 export const myMap = new Map();
 
@@ -33,31 +34,37 @@ function App() {
     myMap.set("bibinetanyahu", new User("bibinetanyahu", "rakbibi", "bibihamelch.gov.il", "BiBi", bibi));
     myMap.set("edenhason", new User("edenhason", "shemishoyatoroti", "edenh@gmail.com", "Eden", eden));
     myMap.set("omeradam", new User("omeradam", "shigramefoert", "omeradam@walla.com", "Omer", omer));
-
     myMap.set("leomessi", new User("leomessi", "101010", "leo@gmail.com", "leo", defaultImage));
     myMap.set("noakirel", new User("noakirel", "111111", "noa@gmail.com", "Noa", squirrel));
 
-    const ofekandgili = myMap.get("ofekyaloz").friends;
-    ofekandgili.set("giligutfeld", []);
-    ofekandgili.get("giligutfeld").push({message: new Message("aaaa", true, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("bbbb", false, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("cccc", false, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("dddd", true, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("eeee", true, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("eeee", true, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("eeee", true, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("eeee", true, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("1234", false, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("1234", false, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("1234", false, new Date(), "text", "")});
-    ofekandgili.get("giligutfeld").push({message: new Message("1234", false, new Date(), "text", "")});
+    const ofekchat = myMap.get("ofekyaloz").friends;
+    ofekchat.set("giligutfeld", []);
+    ofekchat.get("giligutfeld").push({message: new Message("Hi Ofek!", true, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("How are you?", true, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("I'm great, Thanks!", false, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("And how are you?", false, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("Me too", true, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("I like this chat, its an amazing website and designed as well", true, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("Yep, I think its the best chat I ever used", false, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("Its like whatsapp but even better", false, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message("See my new cat", true, new Date(), "text", "")});
+    ofekchat.get("giligutfeld").push({message: new Message(<img src={cat} className={"Chat-Image"}/> , true, new Date(), "photo")});
+    ofekchat.get("giligutfeld").push({message: new Message("Wow! he is so cute", false, new Date(), "text")});
+    ofekchat.get("giligutfeld").push({message: new Message(":)", true, new Date(), "text")});
 
-    const giliandofek = myMap.get("giligutfeld").friends;
-    giliandofek.set("ofekyaloz", []);
+    const gilichat = myMap.get("giligutfeld").friends;
+    gilichat.set("ofekyaloz", []);
+
+    ofekchat.set("omeradam", []);
+    ofekchat.get("omeradam").push({message: new Message("Hi Omer! I want to buy tickets to your show", true, new Date(), "text")});
+    ofekchat.get("omeradam").push({message: new Message("Hi Ofek, because I like you, I will do it for you in sale", false, new Date(), "text")});
+    ofekchat.get("omeradam").push({message: new Message("You can buy 2 tickets in only 800$", false, new Date(), "text")});
+    ofekchat.get("omeradam").push({message: new Message(<audio controls className={"Chat-Audio"}><source src={song}></source></audio>, true, new Date(), "text")});
+    ofekchat.get("omeradam").push({message: new Message("Ok, this is so worthy! I'll take it", true, new Date(), "text")});
 
     const [user, setUser] = useState({username: "", password: ""});
     const [error, setError] = useState("");
-    let friendslist
+    let friendslist;
     let setFriendsList = function (username) {
         friendslist = myMap.get(username).friends.keys();
     }
