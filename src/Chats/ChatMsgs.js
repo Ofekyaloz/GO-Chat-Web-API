f738b082ee5d2e7a41d19fe503718821bd604a84
ofek1 <ofekyaloz16@gmail.com>
2022-04-26 20:23:38 +0300

    chat message has new appearance :D

--- src/Chats/ChatMsgs.js
+++ src/Chats/ChatMsgs.js
@@ -5,7 +5,7 @@ function ChatMsgs({MessageList}) {
             {MessageList.map((msg, key) => (
                 <li className={msg.message.myMsg ? "d-flex justify-content-end right-msg" : "d-flex justify-content-start"}
                     key={key}>
-                    <div className={msg.message.myMsg ? "right-content" : "left-content"} key={key}>
+                    <div className={msg.message.myMsg ? "right-content chat-bubble chat-bubble-right" : "left-content chat-bubble chat-bubble-left"} key={key}>
                         <div>
                             {msg.message.content}
                         </div>
