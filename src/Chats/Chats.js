f3609c127b1cb3f98be672777d53cbb45cfceb50
ofek1 <ofekyaloz16@gmail.com>
2022-05-25 12:50:35 +0300

    signalr

--- src/Chats/Chats.js
+++ src/Chats/Chats.js
@@ -1,5 +1,4 @@
 import './Chats.css';
-import ChatHistory from './ChatHistory.js'
 import LeftMenu from "./LeftMenu";
 import Toolbox from "./Toolbox";
 import {useEffect, useState} from "react";
@@ -9,12 +8,52 @@ import ChatMsgs from "./ChatMsgs";
 import {wait} from "@testing-library/user-event/dist/utils";
 import defaultImage from "../Pictures/icon-user-default.png"
 import $ from "jquery";
+import {HubConnectionBuilder} from "@microsoft/signalr";
+import DisplayChat from "./DisplayChat"
 
 function Chats({username, nickname, photo, token, Logout}) {
     let [Friend, setFriend] = useState({username: '', server: ''});
 
     const [ContactsList, setContactsList] = useState([]);
+    const [MessageList, setMessageList] = useState([]);
+    const [connectionId, setConnectionId] = useState('');
+    const [newUpdate, setNewUpdate] = useState(0);
 
+    useEffect(() => {
+        (async function () {
+            const connection = new HubConnectionBuilder()
+                .withUrl('https://localhost:7265/MyHub')
+                .withAutomaticReconnect()
+                .build();
+            connection.start()
+                .then(() => {
+                    let tmp = 0
+                    connection.on('MessageReceived', () => {
+                        tmp = tmp + 1
+                        setNewUpdate(tmp)
+                    })
+                    connection.invoke('GetConnectionId').then(function (connectionId) {
+                        setConnectionId(connectionId)
+                    })
+                })
+                .catch(e => console.log('Connection failed: ', e))
+        })();
+    }, [])
+
+    useEffect(() => {
+        $.ajax({
+            url: 'https://localhost:7265/api/users/Connection',
+            type: 'POST',
+            contentType: "application/json",
+            data: JSON.stringify({"connectionId": connectionId}),
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+            },
+        })
+    }, [connectionId])
+
+
+    // Get the contacts
     useEffect(() => {
         (async function () {
 
@@ -28,15 +67,16 @@ function Chats({username, nickname, photo, token, Logout}) {
             })
             setContactsList(data);
         })();
-    }, []);
+    }, [MessageList, newUpdate]);
 
-    const [MessageList, setMessageList] = useState([]);
-
+    // Get messages
     useEffect(() => {
         (async function () {
+            console.log("sss")
             if (Friend.username === '') {
                 return;
             }
+            console.log("bbb")
             const url = 'https://localhost:7265/api/Contacts/' + Friend.username + '/messages'
             const data = await $.ajax({
                 url: url,
@@ -47,26 +87,44 @@ function Chats({username, nickname, photo, token, Logout}) {
                 }
             })
             setMessageList(data);
+            console.log(data)
         })();
-    }, [Friend.username]);
+    }, [Friend.username, newUpdate]);
+
+    // const HistoryList = (Array.from(ContactsList).reverse()).map((contact, key) => {
+    //     return <ChatHistory photo={" "} friendUser={contact.id} server={contact.server} setFriend={setFriend}
+    //                         message={contact.last} friendNickname={contact.name} date={contact.lastdate}/>
+    // });
+
 
     const HistoryList = (Array.from(ContactsList).reverse()).map((contact, key) => {
-        const last = contact.last;
-        // if the chat is undefined or has no message.
-        if (last === undefined || last === '') {
-            return <ChatHistory setFriend={setFriend} server={contact.server} message={" "} date={" "} key={key}
-                                friendUser={contact.id} photo={" "} friendNickname={contact.name}/>
-        }
-
-        return <ChatHistory photo={" "} friendUser={contact.id} server={contact.server} setFriend={setFriend} key={key}
-                            message={contact.last} friendNickname={contact.name} date={contact.lastdate}/>
+        return (
+            <button className="list-group-item list-group-item-action" id={contact.id} data-bs-toggle="list"
+                    onClick={() => DisplayChat(setFriend, contact.id, " ", contact.name, contact.server)} role="tab"
+                    aria-controls="list-home" key={contact.name}>
+                <div>
+                    <div className="row">
+                        <div className="col-3" id={"profile-picture-friend"}>
+                            <img className="UserImage" src={contact.photo} srcSet={defaultImage}/>
+                        </div>
+                        <div className="col-9" id={"friend-details"}>
+                            <div className="row">
+                                <div className="col-8 d-flex ContactName" id={"nickname-friend"}> {contact.name} </div>
+                                <div className="col-4" id={"date-friend"}> {contact.lastdate}</div>
+                            </div>
+                            <div className="history-message" id={"message-friend"}> {contact.last} </div>
+                        </div>
+                    </div>
+                </div>
+            </button>
+        )
     });
 
+
     // Add the new message to the user and his friend and scroll down the chat.
-    const handelAddMessage = (newMessage) => {
+    const handelAddMessage = async (newMessage) => {
         const url = localhost + 'api/Contacts/' + Friend.username + '/Messages';
-        console.log("content =", newMessage.content);
-        $.ajax({
+        await $.ajax({
             url: url,
             type: 'POST',
             contentType: "application/json",
@@ -74,29 +132,24 @@ function Chats({username, nickname, photo, token, Logout}) {
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('Authorization', 'Bearer ' + token)
             },
-            success: function () {
-                $.ajax({
-                    url: url, type: 'GET', contentType: "application/json",
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Authorization', 'Bearer ' + token)
-                    },
-                    success: async function (data) {
-                        console.log(data)
-                        setMessageList(await data)
-                    }
-                })
-            },
-            error: function (e) {
-                console.log(e)
-            }
         })
 
         // transfer
-        const url2 = Friend.server.endsWith('/') ? Friend.server + 'api/transfer' : Friend.server + '/api/transfer';
-        $.ajax({
-            url: url2, type: 'POST', contentType: "application/json", dataType: JSON.stringify({
-                from: username, to: Friend.username, content: newMessage.content
+        const url2 = Friend.server.endsWith('/') ? 'https://' + Friend.server + 'api/transfer' : 'https://' + Friend.server + '/api/transfer';
+        await $.ajax({
+            url: url2, type: 'POST', contentType: "application/json", data: JSON.stringify({
+                "from": username, "to": Friend.username, "content": newMessage.content
             }),
+        })
+
+        await $.ajax({
+            url: url, type: 'GET', contentType: "application/json",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + token)
+            },
+            success: function (data) {
+                setMessageList(data)
+            }
         })
 
         wait(
